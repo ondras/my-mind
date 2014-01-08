8d051621bdd5d6665703856231b7e8527173500e
Ondrej Zara <ondrej.zara@gmail.com>
2014-01-08 16:05:18 +0100

    clipboard

--- js/clipboard.js
+++ js/clipboard.js
@@ -4,6 +4,8 @@ MM.Clipboard = {
 };
 
 MM.Clipboard.copy = function(sourceItem) {
+	this._endCut();
+
 	this._data = sourceItem.clone();
 	this._mode = "copy";
 }
@@ -13,17 +15,21 @@ MM.Clipboard.paste = function(targetItem) {
 
 	switch (this._mode) {
 		case "cut":
+			if (this._data == targetItem || this._data.getParent() == targetItem) { /* abort by pasting on the same node or the parent */
+				this._endCut();
+				return;
+			}
+
 			var item = targetItem;
 			while (!item.isRoot()) {
 				if (item == this._data) { return; } /* moving to a child => forbidden */
 				item = item.getParent();
 			}
 
-			if (this._data.getParent() == targetItem) { return; } /* moving to a parent => noop */
-
 			var action = new MM.Action.MoveItem(this._data, targetItem);
 			MM.App.action(action);
-			this._data = null;
+
+			this._endCut();
 		break;
 
 		case "copy":
@@ -31,9 +37,25 @@ MM.Clipboard.paste = function(targetItem) {
 			MM.App.action(action);
 		break;
 	}
+
 }
 
 MM.Clipboard.cut = function(sourceItem) {
+	this._endCut();
+
 	this._data = sourceItem;
 	this._mode = "cut";
+
+	var node = this._data.getDOM().node;
+	node.classList.add("cut");
+}
+
+MM.Clipboard._endCut = function() {
+	if (this._mode != "cut") { return; }
+
+	var node = this._data.getDOM().node;
+	node.classList.remove("cut");
+
+	this._data = null;
+	this._mode = "";
 }
