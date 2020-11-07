5bb484b5c26dd805618d6156a8476953dc53c3cb
Irfan <m.irfanwatoo@gmail.com>
2020-11-07 20:07:11 +0500

    Fixed shortcuts not working after notes UI is closed

--- my-mind.js
+++ my-mind.js
@@ -4186,6 +4186,7 @@ MM.UI.Notes.prototype.toggle = function() {
 MM.UI.Notes.prototype.close = function() {
 	if (this._node.classList.contains("visible")) {
 		this._node.classList.toggle("visible");
+		MM.Clipboard.focus();
 	}
 }
 
