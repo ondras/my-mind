80fde8b09699c780c00eb10a2a31c7f166314e08
0kyn <0kyn.dev@gmail.com>
2021-10-30 15:39:19 +0200

    Remove __reflect

--- my-mind.js
+++ my-mind.js
@@ -1,7 +1,5 @@
 (() => {
   var __defProp = Object.defineProperty;
-  var __reflectGet = Reflect.get;
-  var __reflectSet = Reflect.set;
   var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
   var __export = (target, all2) => {
     __markAsModule(target);
